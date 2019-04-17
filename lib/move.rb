2192368253c53670c1ace707e2ee97dc05deb18c
Anthony Rurup <tcrurup@gmail.com>
2019-04-17 18:33:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,10 +7,9 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  input.to_i - 1;
+  input.to_i - 1
 end
 
 def move(board, position, char = "x")
-  index = input_to_index(position)
-  board[index] = char
+  board[position] = char
 end
